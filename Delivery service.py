1250a30db50c1e4b43f3b008c08d080077c8ed69
Yausei Ilya <iliaevsei32@gmail.com>
2025-05-03 20:40:06 +0300

    v 2.0

---
+++ Delivery service.py
@@ -0,0 +1,34 @@
+"""A. Служба доставки. ID: ."""
+
+
+def min_platforms(data: list[int], max_weight: int):
+    """
+    Определяет минимальное количество транспортных платформ,
+    необходимое для перевозки всех роботов, описанных в массиве."""
+    # Сортируем масив чтобы воспользоваться методом двух указателей.
+    sorted_data = sorted(data)
+    result = 0
+    # Используем метод двух указателей.
+    left_pointer = 0
+    right_pointer = len(sorted_data) - 1
+    while right_pointer >= left_pointer:
+        weight_sum = sorted_data[left_pointer] + sorted_data[right_pointer]
+        if weight_sum <= max_weight:
+            # Увеличиваем счётчик на 1, сумма этих элементов нам подходит.
+            result += 1
+            right_pointer -= 1
+            left_pointer += 1
+        else:
+            # Увеличиваем счётчик на 1, если число не подходит.
+            result += 1 
+            right_pointer -= 1
+
+    return result
+
+
+if __name__ == '__main__':
+    with open('input.txt', 'r') as file_in:
+        data = [int(i) for i in file_in.readline().split()]
+        max_weight = int(file_in.readline())
+    with open('output.txt', 'w') as file_out:
+        file_out.write(str(min_platforms(data, max_weight)))
